26374b383c99113e8319d64febdc06eb76906521
Jovan Milosevic <milosevic.jovan@uns.ac.rs>
2021-08-19 01:01:26 +0200

    Updated Account model.

--- Cakery_Backend/Cakery_Backend/Models/AccountBindingModels.cs
+++ Cakery_Backend/Cakery_Backend/Models/AccountBindingModels.cs
@@ -48,6 +48,14 @@ namespace Cakery_Backend.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Address { get; set; }
+
     }
 
     public class RegisterExternalBindingModel
