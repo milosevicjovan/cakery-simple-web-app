5a72ce8b9a01cb070a5066ed35e9e64ac758db7b
Jovan Milosevic <milosevic.jovan@uns.ac.rs>
2021-08-19 02:56:43 +0200

    Finished Products controller.

--- Cakery_Backend/Cakery_Backend/Controllers/ProductsController.cs
+++ Cakery_Backend/Cakery_Backend/Controllers/ProductsController.cs
@@ -9,14 +9,17 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using Microsoft.AspNet.Identity;
 
 namespace Cakery_Backend.Controllers
 {
+    [Authorize]
     public class ProductsController : ApiController
     {
         [HttpGet]
         [ResponseType(typeof(ProductDTO))]
         [Route("api/products")]
+        [AllowAnonymous]
         public async Task<IHttpActionResult> GetAllProducts()
         {
             using (Cakery_DbContext db = new Cakery_DbContext())
@@ -27,7 +30,7 @@ namespace Cakery_Backend.Controllers
                     Name = p.Name,
                     Description = p.Description,
                     Price = Math.Round(p.Price, 2),
-                    Promotion = Math.Round(p.Price, 2)
+                    Promotion = Math.Round(p.Promotion ?? 0, 2)
                 }).ToListAsync();
 
                 return Ok(products);
@@ -37,6 +40,7 @@ namespace Cakery_Backend.Controllers
         [HttpGet]
         [ResponseType(typeof(ProductDTO))]
         [Route("api/products/{id}")]
+        [AllowAnonymous]
         public async Task<IHttpActionResult> GetProductById(int id)
         {
             using (Cakery_DbContext db = new Cakery_DbContext())
@@ -44,7 +48,7 @@ namespace Cakery_Backend.Controllers
                 var product = await db.Products.SingleOrDefaultAsync(p => p.ProductID == id);
                 if (product == null)
                 {
-                    return BadRequest("Product not found!");
+                    return BadRequest($"Product with ID={ id } was not found!");
                 }
 
                 ProductDTO productDto = new ProductDTO()
@@ -57,6 +61,82 @@ namespace Cakery_Backend.Controllers
                 };
 
                 return Ok(productDto);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/products")]
+        public async Task<IHttpActionResult> AddProduct([FromBody]Product product)
+        {
+            string userId = User.Identity.GetUserId();
+            bool IsUserAdmin = await IsAdmin(userId);
+
+            if (!IsUserAdmin)
+            {
+                return BadRequest("You are not authorized!");
+            }
+
+            if (product == null)
+            {
+                return BadRequest("Product object is null.");
+            }
+
+            using (Cakery_DbContext db = new Cakery_DbContext())
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("Invalid Model state.");
+                }
+
+                db.Products.Add(product);
+
+                await db.SaveChangesAsync();
+
+                return Created("", $"Product { product.Name } is added.");
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/products/{id}")]
+        public async Task<IHttpActionResult> DeleteProduct(int id)
+        {
+            string userId = User.Identity.GetUserId();
+            bool IsUserAdmin = await IsAdmin(userId);
+
+            if (!IsUserAdmin)
+            {
+                return BadRequest("You are not authorized!");
+            }
+
+            using (Cakery_DbContext db = new Cakery_DbContext())
+            {
+                Product product = await db.Products.SingleOrDefaultAsync(p => p.ProductID == id);
+
+                if (product == null)
+                {
+                    return BadRequest($"Product with ID={ id } was not found!");
+                }
+
+                db.Products.Remove(product);
+
+                await db.SaveChangesAsync();
+
+                return Ok($"Product { product.Name } is deleted.");
+            }
+        }
+
+        private async Task<bool> IsAdmin(string userId)
+        {
+            using (Cakery_DbContext db = new Cakery_DbContext())
+            {
+                Role role = await db.Roles.SingleOrDefaultAsync(r => r.Id.Equals(userId));
+                if (role.Name.Equals("admin"))
+                {
+                    return true;
+                } else
+                {
+                    return false;
+                }
             }
         }
     }
