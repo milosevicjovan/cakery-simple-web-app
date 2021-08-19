e5c4a8eac8bda5ee579b5a805b3b85fefdf9c6cc
Jovan Milosevic <milosevic.jovan@uns.ac.rs>
2021-08-19 03:14:57 +0200

    Added necessary environment variables

--- Cakery/src/environments/environment.ts
+++ Cakery/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  serviceApi: "http://localhost:9072/api/",
+  loginApi: "http://localhost:9072/Token"
 };
 
 /*
